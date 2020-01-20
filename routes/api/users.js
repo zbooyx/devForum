2679b400f43fa420def0afec077e036c111a5368
PZ <zbooyx@gmail.com>
2020-01-21 00:32:57 +0100

    12

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
-
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator');
-const User = require('../../models/User');
 const gravatar = require('gravatar');
+
+const User = require('../../models/User');
+
 // @route   GET api/users
 // @desc    Register user
 // @access  Public
@@ -34,11 +37,7 @@ router.post('/',
                 return res.status(400).json({errors: [{msg: 'User already exist'}]});
             }
 
-            const avatar = gravatar.url(email, {
-                s: '200',
-                r: 'pg',
-                d: 'mm'
-            });
+            const avatar = gravatar.url(email);
 
             user = new User({
                 name,
@@ -52,13 +51,27 @@ router.post('/',
             user.password = await bcrypt.hash(password, salt);
 
             await user.save();
-            //return jsonwebtoken
-            res.send('User Registered');
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            };
+
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {expiresIn: 360000},
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({token})
+                }
+            );
+
         } catch (err) {
             console.error(err.message);
             res.status(500).send('ServerError');
         }
     });
-
 
 module.exports = router;

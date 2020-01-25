97bd0c876be3f7ec2b170b260f1dabe87a44406e
PZ <zbooyx@gmail.com>
2020-01-25 23:23:46 +0100

    22 add & del profile edu PUT & DELETE

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -238,4 +238,83 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
     }
 );
 
+
+// @route   PUT api/profile/edu
+// @desc    Add profile edu
+// @access  private
+
+router.put('/education', [
+        auth,
+        [
+            check('school', 'school is required')
+                .not()
+                .isEmpty(),
+            check('degree', 'degree is required')
+                .not()
+                .isEmpty(),
+            check('from', 'from is required')
+                .not()
+                .isEmpty(),
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+        const newEdu = {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        };
+        try {
+            const profile = await Profile.findOne({user: req.user.id});
+
+            profile.education.unshift(newEdu);
+            await profile.save();
+            res.json(profile)
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error')
+        }
+
+    });
+
+// @route   DELETE api/profile/education/:edu_id
+// @desc    DELETE du from profile
+// @access  private
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+        try {
+            // remove profile
+            const profile = await Profile.findOne({user: req.user.id});
+            //Get the removed index
+            const removeIndex = profile.education
+                .map(item => item.id)
+                .indexOf(req.params.edu_id);
+            profile.education.splice(removeIndex, 1);
+
+            await profile.save();
+
+            res.json(profile);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Server error');
+        }
+    }
+);
+
 module.exports = router;

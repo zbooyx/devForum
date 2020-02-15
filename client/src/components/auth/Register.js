904df9342936aeef23754f9cddcbca4a9bb8a1c3
pawel <pawel.zboinski@neoteric.eu>
2020-02-15 17:11:51 +0100

    35 add axios request

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useState} from "react";
+// import axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -10,12 +11,33 @@ const Register = () => {
 
   const {name, email, password, password2} = formData;
   const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
-  const onSubmit = e => {
+  const onSubmit = async e => {
     e.preventDefault();
     if (password !== password2) {
       console.log('Pass do not match')
     } else {
-      console.log(formData, e)
+
+      // AXIOS
+      // const newUser = {
+      //   name,
+      //   email,
+      //   password
+      // };
+      //
+      // try {
+      //   const config = {
+      //     headers: {
+      //       'Content-Type': 'application/json'
+      //     }
+      //   };
+      //
+      //   const body = JSON.stringify(newUser);
+      //   const res = await axios.post('/api/users', body, config);
+      //   console.log(res.data);
+      // } catch (err) {
+      //   console.log(err.response.data)
+      //
+      // }
     }
   };
 

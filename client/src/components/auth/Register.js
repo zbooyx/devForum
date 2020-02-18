ba3c9f190720706d1ec77d1d34fef2517f191e4c
pawel <pawel.zboinski@neoteric.eu>
2020-02-18 17:18:32 +0100

    40 auth reducer and register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,9 +2,10 @@ import React, {Fragment, useState} from "react";
 import {connect} from 'react-redux';
 import {Link} from "react-router-dom";
 import {setAlert} from "../../actions/alert";
+import {register} from "../../actions/auth";
 import PropTypes from 'prop-types';
 
-const Register = ({setAlert}) => {
+const Register = ({setAlert, register}) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -20,7 +21,8 @@ const Register = ({setAlert}) => {
       // console.log('Pass do not match');
       setAlert('Passwords do not match', 'danger');
     } else {
-      console.log('success')
+      console.log('success');
+      register({name, email, password});
     }
   };
 
@@ -36,7 +38,7 @@ const Register = ({setAlert}) => {
             name="name"
             value={name}
             onChange={e => onChange(e)}
-            required
+            // required
           />
         </div>
         <div className="form-group">
@@ -57,7 +59,7 @@ const Register = ({setAlert}) => {
             type="password"
             placeholder="Password"
             name="password"
-            minLength="6"
+            // minLength="6"
             value={password}
             onChange={e => onChange(e)}
           />
@@ -67,7 +69,7 @@ const Register = ({setAlert}) => {
             type="password"
             placeholder="Confirm Password"
             name="password2"
-            minLength="6"
+            // minLength="6"
             value={password2}
             onChange={e => onChange(e)}
           />
@@ -82,10 +84,11 @@ const Register = ({setAlert}) => {
 };
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 };
 
 export default connect(
   null,
-  {setAlert}
+  {setAlert, register}
 )(Register);

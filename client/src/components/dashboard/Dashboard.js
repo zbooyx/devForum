2c0af2e2acf666544736f69355fca33a8ac40bf3
PZ <zbooyx@gmail.com>
2020-04-13 23:33:45 +0200

    45 axios for getting profile data

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,7 +1,15 @@
-import React from "react";
-// import PropTypes from 'prop-types';
+import React, {useEffect} from "react";
+import PropTypes from 'prop-types';
+import {connect} from "react-redux";
+import mapStateToProps from "react-redux/lib/connect/mapStateToProps";
+import {getCurrentProfile} from "../../actions/profile";
+import auth from "../../reducers/auth";
+import profile from "../../reducers/profile";
 
-const Dashboard = props => {
+const Dashboard = ({getCurrentProfile, auth, profile}) => {
+    useEffect(() => {
+        getCurrentProfile();
+    }, []);
     return (
         <div>
             Dashboard
@@ -9,6 +17,15 @@ const Dashboard = props => {
     )
 }
 
-Dashboard.propTypes = {}
+Dashboard.propTypes = {
+    getCurrentProfile: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+    profile: PropTypes.object.isRequired,
+}
 
-export default Dashboard
+const mapToStateProps = state => ({
+    auth: state.auth,
+    profile: state.profile
+});
+
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
